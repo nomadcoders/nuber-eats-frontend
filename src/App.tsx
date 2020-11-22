8f8aa1314955a6e045403f8b10a49f60c105d6cb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-11-22 14:39:00 +0900

    14.3 Apollo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className=" min-h-full bg-red-500 h-full">
-      <h1 className=" text-xl text-white ">Hello Tailwind</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
+import { client } from "./apollo";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
