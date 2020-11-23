82ea76ebc3051fa294066d8179663ea4e5621e92
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-11-23 15:49:45 +0900

    15.4 Form Design

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export const Login = () => {
-  return <span>Login</span>;
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+        <h3 className="text-2xl text-gray-800">Log In</h3>
+        <form className="flex flex-col mt-5 px-5">
+          <input
+            placeholder="Email"
+            className=" bg-gray-100 shadow-inner   border-2 focus:border-opacity-60 focus:border-green-600 focus:outline-none mb-3 py-3 px-5 rounded-lg"
+          />
+          <input
+            placeholder="Password"
+            className=" bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600  py-3 px-5 rounded-lg"
+          />
+          <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
