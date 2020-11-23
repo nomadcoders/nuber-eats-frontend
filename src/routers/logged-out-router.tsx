9765529bfb330302a57f878ffbf5f33288de9e52
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-11-23 15:09:56 +0900

    15.2 React Hook Form part Two

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  password: string;
+}
+
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm();
+  const { register, watch, handleSubmit, errors } = useForm<IForm>();
   const onSubmit = () => {
     console.log(watch());
   };
   const onInvalid = () => {
     console.log("cant create account");
   };
-  console.log(errors);
   return (
     <div>
       <h1>Logged Out</h1>
@@ -24,6 +28,14 @@ export const LoggedOutRouter = () => {
             type="email"
             placeholder="email"
           />
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="font-bold text-red-600">Only gmail allowed</span>
+          )}
         </div>
         <div>
           <input
