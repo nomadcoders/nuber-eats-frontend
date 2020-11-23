c7c02779436c997f558b97f252e2f8c23e233d69
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-11-23 15:22:05 +0900

    15.3 Router and @types

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,53 +1,19 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-
-interface IForm {
-  email: string;
-  password: string;
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CreateAccount } from "../pages/create-account";
+import { Login } from "../pages/login";
 
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit, errors } = useForm<IForm>();
-  const onSubmit = () => {
-    console.log(watch());
-  };
-  const onInvalid = () => {
-    console.log("cant create account");
-  };
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            ref={register({
-              required: "This is required",
-              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="font-bold text-red-600">
-              {errors.email?.message}
-            </span>
-          )}
-          {errors.email?.type === "pattern" && (
-            <span className="font-bold text-red-600">Only gmail allowed</span>
-          )}
-        </div>
-        <div>
-          <input
-            ref={register({ required: true })}
-            name="password"
-            type="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-yellow-300 text-white">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
