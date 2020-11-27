f5f204b14578c376f327c370e59372f3a92dea2b
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-11-27 20:50:07 +0900

    16.1 Verifying Email part Two

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,5 +1,6 @@
-import { gql, useMutation } from "@apollo/client";
+import { gql, useApolloClient, useMutation } from "@apollo/client";
 import React, { useEffect } from "react";
+import { useMe } from "../../hooks/useMe";
 import {
   verifyEmail,
   verifyEmailVariables,
@@ -15,19 +16,41 @@ const VERIFY_EMAIL_MUTATION = gql`
 `;
 
 export const ConfirmEmail = () => {
-  const [verifyEmail, { loading: verifyingEmail }] = useMutation<
-    verifyEmail,
-    verifyEmailVariables
-  >(VERIFY_EMAIL_MUTATION);
+  const { data: userData } = useMe();
+  const client = useApolloClient();
+  const onCompleted = (data: verifyEmail) => {
+    const {
+      verifyEmail: { ok },
+    } = data;
+    if (ok && userData?.me.id) {
+      client.writeFragment({
+        id: `User:${userData.me.id}`,
+        fragment: gql`
+          fragment VerifiedUser on User {
+            verified
+          }
+        `,
+        data: {
+          verified: true,
+        },
+      });
+    }
+  };
+  const [verifyEmail] = useMutation<verifyEmail, verifyEmailVariables>(
+    VERIFY_EMAIL_MUTATION,
+    {
+      onCompleted,
+    }
+  );
   useEffect(() => {
     const [_, code] = window.location.href.split("code=");
-    /*     verifyEmail({
+    verifyEmail({
       variables: {
         input: {
           code,
         },
       },
-    }); */
+    });
   }, []);
   return (
     <div className="mt-52 flex flex-col items-center justify-center">
