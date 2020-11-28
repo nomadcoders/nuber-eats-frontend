4b67cced30b99afea23b24dab978897d7e9e9300
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-11-28 19:33:00 +0900

    17.1 Categories Style

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { url } from "inspector";
 import React from "react";
 import {
   restaurantsPageQuery,
@@ -38,7 +39,7 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Restaurants = () => {
-  const { data, loading, error } = useQuery<
+  const { data, loading } = useQuery<
     restaurantsPageQuery,
     restaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
@@ -48,5 +49,32 @@ export const Restaurants = () => {
       },
     },
   });
-  return <h1>Restaurants</h1>;
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md border-0 w-3/12"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-2xl mx-auto mt-8">
+          <div className="flex justify-around max-w-sm mx-auto ">
+            {data?.allCategories.categories?.map((category) => (
+              <div className="flex flex-col items-center cursor-pointer">
+                <div
+                  className="w-14 h-14 bg-cover hover:bg-gray-100 rounded-full"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
