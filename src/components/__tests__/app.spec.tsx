17a9ed95ae21f7900d995f1debb61acfec5d77b1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-01 14:20:15 +0900

    18.1 App Tests

---
+++ src/components/__tests__/app.spec.tsx
@@ -0,0 +1,29 @@
+import { render, waitFor } from "@testing-library/react";
+import React from "react";
+import { isLoggedInVar } from "../../apollo";
+import { App } from "../app";
+
+jest.mock("../../routers/logged-out-router", () => {
+  return {
+    LoggedOutRouter: () => <span>logged-out</span>,
+  };
+});
+jest.mock("../../routers/logged-in-router", () => {
+  return {
+    LoggedInRouter: () => <span>logged-in</span>,
+  };
+});
+
+describe("<App />", () => {
+  it("renders LoggedOutRouter", () => {
+    const { getByText } = render(<App />);
+    getByText("logged-out");
+  });
+  it("renders LoggedInRouter", async () => {
+    const { getByText, debug } = render(<App />);
+    await waitFor(() => {
+      isLoggedInVar(true);
+    });
+    getByText("logged-in");
+  });
+});
