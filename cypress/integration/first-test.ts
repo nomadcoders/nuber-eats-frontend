9a69a978bac8b5f8cf5a4937708d40a7b1758270
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-02 14:51:04 +0900

    19.1 Our First Cypress Test

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,7 +1,22 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000")
-      .title()
-      .should("eq", "Login | Nuber Eats");
+describe("Log In", () => {
+  it("should see login page", () => {
+    cy.visit("/").title().should("eq", "Login | Nuber Eats");
+  });
+  it("can fill out the form", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("nico@nomadcoders.co")
+      .findByPlaceholderText(/password/i)
+      .type("12345")
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none");
+    // to do (can log in)
+  });
+  it("can see email / password validation errors", () => {
+    cy.visit("/")
+      .findByPlaceholderText(/email/i)
+      .type("bad@email")
+      .findByRole("alert")
+      .should("have.text", "Please enter a valid email");
   });
 });
