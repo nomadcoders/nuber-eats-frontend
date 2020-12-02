e15d1aa7f9928bbafd6001470cda129f0a5b5556
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-02 15:05:01 +0900

    19.2 Login E2E

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,22 +1,32 @@
 describe("Log In", () => {
+  const user = cy;
   it("should see login page", () => {
-    cy.visit("/").title().should("eq", "Login | Nuber Eats");
-  });
-  it("can fill out the form", () => {
-    cy.visit("/")
-      .findByPlaceholderText(/email/i)
-      .type("nico@nomadcoders.co")
-      .findByPlaceholderText(/password/i)
-      .type("12345")
-      .findByRole("button")
-      .should("not.have.class", "pointer-events-none");
-    // to do (can log in)
+    user.visit("/").title().should("eq", "Login | Nuber Eats");
   });
   it("can see email / password validation errors", () => {
-    cy.visit("/")
-      .findByPlaceholderText(/email/i)
-      .type("bad@email")
-      .findByRole("alert")
-      .should("have.text", "Please enter a valid email");
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("bad@email");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("bad@email.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("nico@nomadcoders.co");
+    user.findByPlaceholderText(/password/i).type("121212");
+    user
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click();
+    user.window().its("localStorage.nuber-token").should("be.a", "string");
+  });
+  it("sign up", () => {
+    user.visit("/create-account");
   });
 });
