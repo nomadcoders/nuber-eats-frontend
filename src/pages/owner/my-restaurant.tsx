b23ec0f084f6954fa1397865c403d984242f8f56
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-03 19:33:03 +0900

    20.14 Victory Charts part Two

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, useParams } from "react-router-dom";
 import { Dish } from "../../components/dish";
-import { VictoryAxis, VictoryBar, VictoryChart } from "victory";
+import {
+  VictoryAxis,
+  VictoryBar,
+  VictoryChart,
+  VictoryLabel,
+  VictoryPie,
+} from "victory";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { useMe } from "../../hooks/useMe";
 import {
@@ -90,6 +96,19 @@ export const MyRestaurant = () => {
       });
     }
   };
+  const chartData = [
+    { x: 1, y: 3000 },
+    { x: 2, y: 1500 },
+    { x: 3, y: 4250 },
+    { x: 4, y: 1250 },
+    { x: 5, y: 2300 },
+    { x: 6, y: 7150 },
+    { x: 7, y: 6830 },
+    { x: 8, y: 6830 },
+    { x: 9, y: 6830 },
+    { x: 10, y: 6830 },
+    { x: 11, y: 6830 },
+  ];
   return (
     <div>
       <Helmet>
@@ -140,22 +159,10 @@ export const MyRestaurant = () => {
         <div className="mt-20 mb-10">
           <h4 className="text-center text-2xl font-medium">Sales</h4>
           <div className=" max-w-lg w-full mx-auto">
-            <VictoryChart domainPadding={20}>
-              <VictoryAxis
-                label="Amount of Money"
-                dependentAxis
-                tickValues={[20, 30, 40, 50, 60]}
-              />
-              <VictoryAxis label="Days of Life" />
-              <VictoryBar
-                data={[
-                  { x: 10, y: 20 },
-                  { x: 20, y: 5 },
-                  { x: 35, y: 55 },
-                  { x: 45, y: 99 },
-                ]}
-              />
-            </VictoryChart>
+            <VictoryPie
+              data={chartData}
+              colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
+            />
           </div>
         </div>
       </div>
