53f9dd8473b2eb81fb97197de8f00ad6cb2b51b3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-04 20:59:20 +0900

    23.9 Painting Directions

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -49,6 +49,39 @@ export const Dashboard = () => {
     setMap(map);
     setMaps(maps);
   };
+  const onGetRouteClick = () => {
+    if (map) {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer({
+        polylineOptions: {
+          strokeColor: "#000",
+          strokeOpacity: 1,
+          strokeWeight: 58,
+        },
+      });
+      directionsRenderer.setMap(map);
+      directionsService.route(
+        {
+          origin: {
+            location: new google.maps.LatLng(
+              driverCoords.lat,
+              driverCoords.lng
+            ),
+          },
+          destination: {
+            location: new google.maps.LatLng(
+              driverCoords.lat + 0.05,
+              driverCoords.lng + 0.05
+            ),
+          },
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (result) => {
+          directionsRenderer.setDirections(result);
+        }
+      );
+    }
+  };
   return (
     <div>
       <div
@@ -59,16 +92,15 @@ export const Dashboard = () => {
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
           defaultZoom={16}
-          draggable={false}
+          draggable={true}
           defaultCenter={{
             lat: 36.58,
             lng: 125.95,
           }}
           bootstrapURLKeys={{ key: "AIzaSyDBoOMuQsFzQJJ-UAxEItgE3K3-JfH7opA" }}
-        >
-          <Driver lat={driverCoords.lat} lng={driverCoords.lng} />
-        </GoogleMapReact>
+        ></GoogleMapReact>
       </div>
+      <button onClick={onGetRouteClick}>Get route</button>
     </div>
   );
 };
