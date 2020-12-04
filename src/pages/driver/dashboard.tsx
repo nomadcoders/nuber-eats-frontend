f5cd1cec03daa2b99c4924dae64da49bd4442b85
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-12-04 17:12:45 +0900

    23.5 Driver Dashboard part One

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -1,13 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import GoogleMapReact from "google-map-react";
 
 export const Dashboard = () => {
-  const onWatchSucces = (coord: Position) => {
-    console.log(coord);
-  };
-  useEffect(() => {
-    navigator.geolocation.watchPosition(onWatchSucces);
-  }, []);
   return (
     <div>
       <div
@@ -15,14 +9,15 @@ export const Dashboard = () => {
         style={{ width: window.innerWidth, height: "95vh" }}
       >
         <GoogleMapReact
-          defaultZoom={10}
-          draggable={false}
+          defaultZoom={20}
           defaultCenter={{
             lat: 59.95,
             lng: 30.33,
           }}
           bootstrapURLKeys={{ key: "AIzaSyDBoOMuQsFzQJJ-UAxEItgE3K3-JfH7opA" }}
-        ></GoogleMapReact>
+        >
+          <h1>hello</h1>
+        </GoogleMapReact>
       </div>
     </div>
   );
